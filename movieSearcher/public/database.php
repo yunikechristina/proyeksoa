32e70936b4036c0b53ac784ad99c936358d907be
yunikechristina <yunikec@gmail.com>
2018-07-04 10:51:21 +0700

    update detail

--- movieSearcher/public/database.php
+++ movieSearcher/public/database.php
@@ -1,10 +1,11 @@
 <?php
 
 class Database {
-    public $con;
+    public $con, $cin;
 
     function __construct(){
         $this->con = mysqli_connect("localhost", "root", "", "proyek_soa");
+        $this->cin = mysqli_connect("192.168.254.216", "root", "", "trailer_soa");
     }
 };
 
